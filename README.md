eb51a3c52da64c1f2273ffae689945da30c5127a
sebastian <segut10@hotmail.com>
2025-06-19 19:22:38 -0500

    subiend parte 5 del proyecto

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/Sgutierrezgf/fullstack-javascript-project-98/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Sgutierrezgf/fullstack-javascript-project-98/actions)
 
 [![Maintainability](https://qlty.sh/badges/812b97e6-9d66-483d-b091-c6990ecf94dc/maintainability.svg)](https://qlty.sh/gh/Sgutierrezgf/projects/fullstack-javascript-project-98)
+
+grabacion del juego ->  https://asciinema.org/a/BJPf7htyt31MQrixtMAGybc2E
---
+++ bin/brain-even.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+import runGame from '../src/even.js';
+
+console.log('¡Bienvenido a Brain Games!');
+const userName = readlineSync.question('¿Cuál es tu nombre? ');
+console.log(`¡Hola, ${userName}!`);
+
+console.log('Responde "yes" si el número es par, de lo contrario responde "no".');
+
+
+let correctAnswersInARow = 0;
+
+while (correctAnswersInARow < 3) {
+    const { randomNumber, correctAnswer } = runGame();
+
+
+    console.log(`Pregunta: ¿Es ${randomNumber} par?`);
+
+
+    const userAnswer = readlineSync.question('Tu respuesta: ').toLowerCase();
+
+
+    if (userAnswer === correctAnswer) {
+        console.log('¡Correcto!');
+        correctAnswersInARow++;
+    } else {
+        console.log(`'${userAnswer}' es una respuesta incorrecta ;(. La respuesta correcta era '${correctAnswer}'.`);
+        console.log('¡Intentémoslo de nuevo, ' + userName + '!');
+        correctAnswersInARow = 0;
+    }
+}
+
+console.log(`🏆 ¡Felicidades, ${userName}! Has respondido correctamente 3 veces seguidas.`);
