77e402ff5d6321da02137ea304da97b19b9ae8e9
sebastian <segut10@hotmail.com>
2025-06-25 08:17:32 -0500

    subiendo cambio de pregunta

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,5 +3,5 @@ import readlineSync from 'readline-sync';
 
 console.log("¡Bienvenido a Brain Games!");
 
-const name = readlineSync.question('Cual es tu nombre?: ');
+const name = readlineSync.question('¿Cuál es tu nombre?: ');
 console.log(`¡Hola, ${name}!`);
