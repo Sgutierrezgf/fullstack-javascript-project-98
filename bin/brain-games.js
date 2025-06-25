ea87ef144a7398ca57031adb9e1ad92381959b41
sebastian <segut10@hotmail.com>
2025-06-25 08:14:52 -0500

    subiendo cambio de mensaje

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-console.log("Welcome to the Brain Games!");
+
+console.log("¡Bienvenido a Brain Games!");
 
 const name = readlineSync.question('Cual es tu nombre?: ');
 console.log(`¡Hola, ${name}!`);
-
